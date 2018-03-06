fe34630c619836c51f7c8daf656aefdd53a79655
Tomáš Krejčí <tomas@differencemakers.cz>
2018-03-06 10:40:22 +0100

    …

---
+++ src/OperatingSystemDetector.php
@@ -0,0 +1,29 @@
+<?php
+namespace OsDetector;
+
+class OperatingSystemDetector
+{
+	/**
+	 * @return string
+	 */
+	public static function getOs(): string
+	{
+		return PHP_OS;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public static function isLinux(): bool
+	{
+		return strncasecmp(PHP_OS, 'LINUX', 5) === 0;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public static function isWindows(): bool
+	{
+		return strncasecmp(PHP_OS, 'WIN', 3) === 0;
+	}
+}
